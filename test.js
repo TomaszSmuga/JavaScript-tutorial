0ac790ae1a81ae7af550d2eafbd5e3281ec441da
Tomek <smuga2505tomek>
2022-09-06 17:08:03 +0200

    Functions

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
-var links = document.getElementsByTagName("a")
+function getAverage (a,b,c,d,e,f){
 
-for ( i  = 0, i < links.length; i++;) {
+    var average = (a + b + c + d + e + f)/2;
 
-    links[i].className = "Link-" + i;
+    console.log(average);
 
+    return average;
 }
+
+var myResult = getAverage(3,5,6,4,3,2)
+
+console.log(" The average is " + myResult)
