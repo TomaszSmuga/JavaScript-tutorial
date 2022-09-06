7f94c1e2d770ad195034b17ba389da8e324e1dcc
Tomek <smuga2505tomek>
2022-09-06 16:55:53 +0200

    Break & Continue

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
-var links = document.getElementsByTagName("a");
+for (i = 0; i < 10; i++){
+    if (i === 5 || i === 3 ){
+        continue;
+    }
+    console.log(i);
 
-for (i = 0; i < links.length; i++) {
-
-   console.log("This is link number" + i);
-
+    if (i === 7 ){
+        break;
+    }
 }
 
-document.write("all links now looped");
+console.log("Loop broken")
