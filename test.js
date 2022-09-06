a5d28051cab39764f232c6e112468faa17a5efb6
Tomek <smuga2505tomek>
2022-09-06 16:24:06 +0200

    Else if

--- test.js
+++ test.js
@@ -1,18 +1,23 @@
 
-var youLikeMeat = false
-var myNum = 10;
+var myAge = 60;
 
-if (youLikeMeat){
+if (myAge > 30){
 
-    document.write("you like meat");
+    document.write("you are over 30");
+
+} else if (myAge > 20){
     
+    document.write("you are over 20");
 
-} else {
+} else if (myAge > 10) {
 
-  document.write("you hate meat")  
+    document.write("you are over 10");
+
+
+} else{
+
+    document.write("you are not over 10");
 
 }
-
-
 
 
