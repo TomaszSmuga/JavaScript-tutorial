78b01c2262050005e5f85cd29a1392df86b4819d
Tomek <smuga2505tomek>
2022-09-06 17:13:17 +0200

    Variable Scope

--- test.js
+++ test.js
@@ -1,12 +1,21 @@
-function getAverage (a,b,c,d,e,f){
+function getAverage (a,b){
 
-    var average = (a + b + c + d + e + f)/2;
+    var average = (a + b)/2; // local variable
 
     console.log(average);
 
     return average;
 }
 
-var myResult = getAverage(3,5,6,4,3,2)
+var myResult = getAverage(3,5); //global variable
 
-console.log(" The average is " + myResult)
+console.log(" The average is " + myResult);
+
+function logResult(){
+
+    console.log(" The average is " + myResult + " inside the function");
+
+
+}
+
+logResult();
