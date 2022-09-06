ea89754d170b4e55429a22ff76a2d05ed0fa244e
Tomek <smuga2505tomek>
2022-09-06 16:19:27 +0200

    If Statements

--- test.js
+++ test.js
@@ -1,5 +1,17 @@
-alert("Hello World!"); 
 
+var youLikeMeat = false
+var myNum = 10;
+
+if (youLikeMeat){
+
+    document.write("you like meat");
+    
+
+} else {
+
+  document.write("you hate meat")  
+
+}
 
 
 
