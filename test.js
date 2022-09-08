6504cfdb911de834f51906e8b879ee1c4324c57b
Tomek <smuga2505tomek>
2022-09-08 12:11:32 +0200

    Introduction to objects

--- test.js
+++ test.js
@@ -1,20 +1,8 @@
-var myArray = [];
+var myString = new String();
 
-myArray[0] = 25
+myString = "hello";
 
-console.log(myArray);
-
-myArray[1] = "bun"
-myArray[2] = 2 * 4
-
-var myArray2 = [10, 20, "uv", (3-5)*2]
-
-console.log(myArray2)
-
-var myArray3 = new Array(5)
-
-console.log(myArray2.sort());
-console.log(myArray2.reverse());
+console.log(myString.length)
 
 
 
