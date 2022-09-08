651300c14cfc583f90e49d55a78637752a1373ab
Tomek <smuga2505tomek>
2022-09-08 12:28:53 +0200

    "This" Keyword

--- test.js
+++ test.js
@@ -1,22 +1,19 @@
-var myArray = new Array();
 
-myArray[0] = 8;
-myArray[1] = "hello";
-
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Tomek";
-myCar.drive = function(){ console.log("now diriving");};
-
-myCar.drive();
 
 var myCar2 = {
             maxSpeed: 50, 
             driver: "Smugeiro", 
             drive: function(speed, time){ 
-            console.log(speed * time + " is the distance");}
+            console.log(speed * time + " is the distance");},
+            logdriver: function(){
+                console.log("driver name is " + this.driver);
+            }
 };
 
+
+
+
+myCar2.logdriver();
 
 console.log(myCar2.maxSpeed);
 myCar2.drive(50, 3);
